bf2d3ebd0e12ee888098b2a8156da842d2b47fb7
Pietro Di Bello <pietro.dibello@xpeppers.com>
2016-01-24 23:36:09 +0100

    Add a first simple test of quality degradation for generic items

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,19 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void generic_items_have_quality_decreasing_by_one_for_each_update() {
+        Item[] items = new Item[] { new Item("foo", 1, 2) };
+
         GildedRose app = new GildedRose(items);
+
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(2, app.items[0].quality);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals(1-1, app.items[0].sellIn);
+        assertEquals(2-1, app.items[0].quality);
     }
+
 
 }
