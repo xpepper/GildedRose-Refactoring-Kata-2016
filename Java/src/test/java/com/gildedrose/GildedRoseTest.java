872e9168b74e43d0a72d9d1208f4d8650bbbd898
Pietro Di Bello <pietro.dibello@xpeppers.com>
2016-01-25 14:12:46 +0100

    Add new test which covers Aged Brie item

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -18,13 +18,13 @@ public class GildedRoseTest {
     public void at_the_end_of_each_day_the_system_lowers_both_values_for_every_item() {
         app.add(aGenericItemWith().quality(2).sellIn(1).build());
 
-        assertEquals(1, app.item(0).sellIn);
-        assertEquals(2, app.item(0).quality);
+        assertEquals(1, firstItem().sellIn);
+        assertEquals(2, firstItem().quality);
 
         app.updateInventory();
 
-        assertEquals(1-1, app.item(0).sellIn);
-        assertEquals(2-1, app.item(0).quality);
+        assertEquals(1 - 1, firstItem().sellIn);
+        assertEquals(2 - 1, firstItem().quality);
     }
 
     @Test
@@ -33,7 +33,7 @@ public class GildedRoseTest {
 
         app.updateInventory();
 
-        assertEquals(0, app.item(0).quality);
+        assertEquals(0, firstItem().quality);
     }
 
     @Test
@@ -42,7 +42,21 @@ public class GildedRoseTest {
 
         app.updateInventory();
 
-        assertEquals(0, app.item(0).quality);
+        assertEquals(0, firstItem().quality);
+    }
+
+    @Test
+    public void AgedBrie_actually_increases_in_Quality_the_older_it_gets() throws Exception {
+        Item agedBrie = new ItemBuilder("Aged Brie").quality(2).sellIn(1).build();
+        app.add(agedBrie);
+
+        app.updateInventory();
+
+        assertEquals(2 + 1, firstItem().quality);
+    }
+
+    private Item firstItem() {
+        return app.item(0);
     }
 
     private ItemBuilder aGenericItemWith() {
