9a290cde2e06db451e2b34057bf4ad318e82242f
Pietro Di Bello <pietro.dibello@xpeppers.com>
2016-01-25 23:30:00 +0100

    Enable the ignored test to catch the current behavior: still need to understand if it's correct or a bug

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,7 +3,6 @@ package com.gildedrose;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class GildedRoseTest {
@@ -56,14 +55,16 @@ public class GildedRoseTest {
         assertEquals(2 + 1, firstItem().quality);
     }
 
-    @Test @Ignore //TODO: is this a wrong case or a bug?
+    @Test
     public void aged_brie_increases_in_quality_even_when_the_sell_by_date_has_passed() throws Exception {
         Item agedBrie = new ItemBuilder("Aged Brie").quality(2).sellIn(-1).build();
         app.add(agedBrie);
 
         app.updateInventory();
 
-        assertEquals(2 + 1, firstItem().quality);
+        // TODO: is this a wrong case or a bug?
+        // assertEquals(2 + 1, firstItem().quality);
+        assertEquals(2 + 2, firstItem().quality);
     }
 
     @Test
